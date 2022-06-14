a6813d1bbb68b3051f1638a99b9abc04ca9d58d9
ASchro1 <aaron243@revature.com>
2022-06-14 16:47:36 -0400

    Transaction service stuff

--- src/main/java/com/revature/controller/TransactionController.java
+++ src/main/java/com/revature/controller/TransactionController.java
@@ -7,11 +7,15 @@ import org.eclipse.jetty.http.HttpStatus;
 
 public class TransactionController {
     public static void postTransaction(Context ctx){
-        Transaction t = ctx.bodyAsClass(Transaction.class);
-        if(!TransactionService.postTransaction(t)){
+        try {
+            Transaction t = ctx.bodyAsClass(Transaction.class);
+            if (!TransactionService.postTransaction(t, ctx.sessionAttribute("user"))) {
+                ctx.status(HttpStatus.BAD_REQUEST_400);
+            } else {
+                ctx.status(HttpStatus.CREATED_201);
+            }
+        }catch(Exception e){
             ctx.status(HttpStatus.BAD_REQUEST_400);
-        }else{
-            ctx.status(HttpStatus.CREATED_201);
         }
     }
 
--- src/main/java/com/revature/service/TransactionService.java
+++ src/main/java/com/revature/service/TransactionService.java
@@ -1,12 +1,92 @@
 package com.revature.service;
 
-import com.revature.models.Transaction;
+import com.revature.dao.AccountDao;
+import com.revature.dao.TransactionDao;
+import com.revature.models.*;
 
 public class TransactionService {
+	private static final AccountDao aDao = new AccountDao();
 
-	public static boolean postTransaction(Transaction t) {
+	private static final TransactionDao tDao = new TransactionDao();
+	public static boolean postTransaction(Transaction t, User u) {
 		// TODO Auto-generated method stub
+		Account a = aDao.getAccountByAccountID(t.getOriginAccount());
+		if(a.getPrimaryOwnerID() == u.getUserID() || a.getJointOwnerID() == u.getUserID()){
+			switch(t.getTransactionType()){
+				case DEPOSIT:
+					return deposit(t, a);
+					break;
+				case WITHDRAWAL:
+					return withdrawal(t, a);
+					break;
+				case TRANSFER:
+					return initTransfer(t, a);
+					break;
+				default:
+					return false;
+			}
+		}
 		return false;
+	}
+
+	private static boolean deposit(Transaction t, Account a){
+		if(t.getTransactionAmount() >= 0){
+			t.setDestinationAccount(a.getAccountID());
+			t.setTransactionStatus(TransactionStatus.APPROVED);
+			a.setBalance(a.getBalance() + t.getTransactionAmount());
+			aDao.updateAccount(a);
+			tDao.insertTransaction(t);
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+
+	private static boolean withdrawal(Transaction t, Account a){
+		if(t.getTransactionAmount() >= 0 && t.getTransactionAmount() <= a.getBalance()){
+			t.setDestinationAccount(a.getAccountID());
+			t.setTransactionStatus(TransactionStatus.APPROVED);
+			a.setBalance(a.getBalance() - t.getTransactionAmount());
+			aDao.updateAccount(a);
+			tDao.insertTransaction(t);
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	private static boolean initTransfer(Transaction t, Account a){
+		if(t.getTransactionAmount() >= 0 && t.getTransactionAmount() <= a.getBalance() && aDao.getAccountByAccountID(t.getDestinationAccount()) != null){
+			t.setTransactionStatus(TransactionStatus.PENDING);
+			a.setBalance(a.getBalance() - t.getTransactionAmount());
+			aDao.updateAccount(a);
+			tDao.insertTransaction(t);
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public static boolean updateTransfer(Transaction t, User u){
+		Account a = aDao.getAccountByAccountID(t.getDestinationAccount());
+		if(t.getTransactionStatus() != TransactionStatus.PENDING && (a.getPrimaryOwnerID() == u.getUserID() || a.getJointOwnerID() == u.getUserID())){
+			Transaction real = tDao.getTransactionByID(t.getTransactionID());
+			real.setTransactionStatus(t.getTransactionStatus());
+			if(real.getTransactionStatus() == TransactionStatus.APPROVED){
+				a.setBalance(a.getBalance() + real.getTransactionAmount());
+				aDao.updateAccount(a);
+				tDao.updateTransactionStatus(real);
+			}else{
+				Account origin = aDao.getAccountByAccountID(real.getOriginAccount());
+				origin.setBalance(origin.getBalance() + real.getTransactionAmount());
+				aDao.updateAccount(origin);
+				tDao.updateTransactionStatus(real);
+			}
+			return true;
+		}else {
+			return false;
+		}
 	}
 
 	public static void getAllTransactions() {
