3f2ad39241c3960b072ade3e9d47e78a4bb95bf2
ooido <false@false.com>
2022-06-18 13:14:05 -0700

    init test infinity+2

--- resources/postgres/init_rewrite.sql
+++ resources/postgres/init_rewrite.sql
@@ -3,15 +3,15 @@
 BEGIN;
 
 CREATE SCHEMA public;
-CREATE TABLE users(
+CREATE TABLE public.users(
                       user_id serial PRIMARY KEY,
                       first_name varchar(30) NOT NULL CHECK(LENGTH(first_name) > 0),
                       last_name varchar(30) NOT NULL CHECK(LENGTH(last_name) > 0),
                       username varchar(30) NOT NULL UNIQUE CHECK(LENGTH(username) > 0),
                       password varchar(30)  NOT NULL CHECK(LENGTH(password) > 0)
 );
-CREATE TYPE a_type AS ENUM ('CHECKING', 'SAVINGS');
-CREATE TABLE accounts(
+CREATE TYPE public.a_type AS ENUM ('CHECKING', 'SAVINGS');
+CREATE TABLE public.accounts(
                          account_id serial PRIMARY KEY,
                          primary_owner_id int REFERENCES users(user_id),
                          joint_owner_id int REFERENCES users(user_id),
@@ -19,13 +19,13 @@ CREATE TABLE accounts(
                          balance numeric NOT NULL,
                          approved boolean DEFAULT FALSE
 );
-CREATE TABLE employees(
+CREATE TABLE public.employees(
                           fk_user_id int PRIMARY KEY REFERENCES users(user_id) ON DELETE CASCADE,
                           authorized boolean NOT NULL
 );
-CREATE TYPE t_type AS ENUM ('WTIHDRAWAL', 'DEPOSIT', 'TRANSFER');
-CREATE TYPE t_status AS ENUM ('PENDING', 'APPROVED', 'DENIED');
-create table transactions(
+CREATE TYPE public.t_type AS ENUM ('WTIHDRAWAL', 'DEPOSIT', 'TRANSFER');
+CREATE TYPE public.t_status AS ENUM ('PENDING', 'APPROVED', 'DENIED');
+create table public.transactions(
                              transaction_id serial PRIMARY KEY,
                              transaction_type t_type NOT NULL,
                              origin_account_id int NOT NULL references accounts(account_id),
