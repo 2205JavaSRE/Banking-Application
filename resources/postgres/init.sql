209690f2e8e204d00ad90948bd510fc690d3a3c7
ooido <false@false.com>
2022-06-18 12:44:10 -0700

    init test 959

--- resources/postgres/init.sql
+++ resources/postgres/init.sql
@@ -33,18 +33,18 @@ CREATE DATABASE postgres WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'e
 
 ALTER DATABASE postgres OWNER TO postgres;
 
-\connect postgres
+--\connect postgres
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+--SET statement_timeout = 0;
+--SET lock_timeout = 0;
+--SET idle_in_transaction_session_timeout = 0;
+--SET client_encoding = 'UTF8';
+--SET standard_conforming_strings = on;
+--SELECT pg_catalog.set_config('search_path', '', false);
+--SET check_function_bodies = false;
+--SET xmloption = content;
+--SET client_min_messages = warning;
+--SET row_security = off;
 
 --
 -- TOC entry 3903 (class 0 OID 0)
