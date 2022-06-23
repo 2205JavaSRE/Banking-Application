371f3eabe4467b78a690d0adec5defe2da4edc09
Braylen Strain <76047181+braylenstrain@users.noreply.github.com>
2022-06-22 21:31:31 -0600

    Created custom exception for bad DB connection

--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -8,19 +8,20 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static String URL = System.getenv("db_url");
+    private static String URL = System.getenv("");
     private static String USERNAME = System.getenv("db_username");
     private static String PASSWORD = System.getenv("db_password");
 
     private static Connection connection;
 
-    public static Connection getConnection() {
+    public static Connection getConnection() throws DatabaseConnectionFailureException {
 
         try {
             connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
         } catch (SQLException e) {
             e.printStackTrace();
             MainDriver.monitor.incrementSqlCounter();
+            throw new DatabaseConnectionFailureException("Attempt to connect to the database was unsuccessful.");
         }
 
         return connection;
